eebb72ac6a39c41562d097e527d67bcdcca9d218
FRQ user slendl <slendl@frqdevvm.(none)>
2015-05-26 10:15:42 +0200

    init svn config

---
+++ bin/svn-merge-vim.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+
+BASE=${1}
+THEIRS=${2}
+MINE=${3}
+MERGED=${4}
+WCPATH=${5}
+
+gvimdiff $MINE $MERGED $THEIRS \
+   -c "setl stl=MINE | wincmd W | setl stl=THEIRS | wincmd W | setl stl=MERGE"
+
