c691084af902bba1fce1fda4d3fcd89b1adcb6ba
Stefan Lendl <ste.lendl@gmail.com>
2015-08-06 10:19:11 +0200

    diffwrap turn order around

--- .local/bin/diffwrap.sh
+++ .local/bin/diffwrap.sh
@@ -6,8 +6,8 @@ DIFF="/usr/bin/vimdiff"
 
 # Subversion provides the paths we need as the sixth and seventh
 # parameters.
-LEFT=${6}
-RIGHT=${7}
+LEFT=${7}
+RIGHT=${6}
 
 # Call the diff command (change the following line to make sense for
 # your merge program).
